62a5a075d5acf258e337f8b4cdacb139c1d94756
amjed.anver <amjed.anver@cloudsolution-sa.com>
2022-12-25 12:07:22 +0300

    initial commit

---
+++ src/main/java/com/example/mirathinventory/MirathInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.mirathinventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MirathInventoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MirathInventoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mirathinventory/MirathInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mirathinventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MirathInventoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
