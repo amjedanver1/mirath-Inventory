3cbcfde003d1f11a2de2ec53cce9adb1b0a8d444
amjed.anver <amjed.anver@cloudsolution-sa.com>
2022-12-27 13:00:05 +0300

    Added services and controllers

--- src/main/java/com/example/mirathinventory/MirathInventoryApplication.java
+++ src/main/java/com/example/mirathinventory/MirathInventoryApplication.java
@@ -2,8 +2,11 @@ package com.example.mirathinventory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
+//@ComponentScan(basePackages = {"com.example.mirathinventory"})
 public class MirathInventoryApplication {
 
     public static void main(String[] args) {
